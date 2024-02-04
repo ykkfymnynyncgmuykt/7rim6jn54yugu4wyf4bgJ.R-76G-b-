ee0377eafc527d340d06372457ebf2487f7c089e
ykkfymnynyncgmuykt <158807514+ykkfymnynyncgmuykt@users.noreply.github.com>
2024-02-04 13:45:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    grabCursor: true,
+    autoplay: true,
+    effect: 'cube', 
+    speed: 1000,
+  })
+
+  // fade, flip, cube, coverflow, cards
